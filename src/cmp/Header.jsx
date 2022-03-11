7b8a3e117b18c45ff73e2e3a14b3fda6c514e329
bubandrei <bubeleu@gmail.com>
2022-03-11 15:44:46 +0300

    began

---
+++ src/cmp/Header.jsx
@@ -0,0 +1,8 @@
+const Header = () => {
+    return (
+        <header className='header'>
+        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/NASA_logo.svg/800px-NASA_logo.svg.png'></img>
+      </header>
+    )
+}
+export default Header
---
+++ src/cmp/Nav.jsx
@@ -0,0 +1,22 @@
+const Nav = () =>{
+    return (
+    <nav className='nav'>
+    <div>
+      <a>Prfile</a>
+    </div>
+    <div>
+      <a>Message</a>
+    </div>
+    <div>
+      <a>News</a>
+    </div>
+    <div>
+      <a>Music</a>
+    </div>
+    <div>
+      <a>Seings</a>
+    </div>
+  </nav>
+    )
+}
+export default Nav
