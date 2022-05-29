11282a22b79d6acb9a0c68de9b63064f1bebe4a7
a.bubelev <bubeleu@gmail.com>
2022-05-29 15:16:47 +0300

    fix dialogsReducer

--- src/redux/dialogsReducer.js
+++ src/redux/dialogsReducer.js
@@ -28,20 +28,21 @@ let initialState = {
 }
 
 const dialogsReducer = (state = initialState, action) => {
+
     switch (action.type) {
         case UPDATE_NEW_MESSAGE_BODY: {
-            let stateCopy = { ...state };
-            stateCopy.newMessageBody = action.body;
-            return stateCopy;
+            return {
+                ...state,
+                newMessageBody: action.body
+            };
         }
-
         case SEND_MESSAGE: {
-            let stateCopy = {...state }
-            let body = stateCopy.newMessageBody;
-            stateCopy.messages = [...state.messages];
-            stateCopy.messages.push({ id: 6, message: body });
-            stateCopy.newMessageBody = '';
-            return stateCopy;
+            let body = state.newMessageBody;
+            return {
+                ...state,
+                newMessageBody: '',
+                messages: [...state.messages, { id: 6, message: body }]
+            }
         }
         default:
             return state;
