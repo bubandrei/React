9e60ce41cd791a2913ffbc022bc84f135fdb1d7d
a.bubelev <bubeleu@gmail.com>
2022-06-30 21:15:19 +0300

    axios

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,15 +1,18 @@
+import axios from "axios";
 import React from "react";
 import styles from './users.module.css';
 
 let Users = (props) => {
 
     if (props.users.length === 0) {
-        props.setUsers([
-            { id: 1, fotoUrl: 'https://filestore.community.support.microsoft.com/api/images/f2e55cbf-8316-4d3a-9412-ecd8194b2a72?upload=true', followed: false, fullname: 'Andrei', status: 'I am a boss', location: { city: 'Warszawa', country: 'Poland' } },
-            { id: 2, fotoUrl: 'https://filestore.community.support.microsoft.com/api/images/f2e55cbf-8316-4d3a-9412-ecd8194b2a72?upload=true', followed: true, fullname: 'Sveta', status: 'I am a boss too', location: { city: 'Wrocław', country: 'Poland' } },
-            { id: 3, fotoUrl: 'https://filestore.community.support.microsoft.com/api/images/f2e55cbf-8316-4d3a-9412-ecd8194b2a72?upload=true', followed: false, fullname: 'Amina', status: 'I am a big boss', location: { city: 'Krakow', country: 'Poland' } }
-        ]
-        )
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+        .then((response)=> props.SetUsers())
+        // props.setUsers([
+        //     { id: 1, fotoUrl: 'https://filestore.community.support.microsoft.com/api/images/f2e55cbf-8316-4d3a-9412-ecd8194b2a72?upload=true', followed: false, fullname: 'Andrei', status: 'I am a boss', location: { city: 'Warszawa', country: 'Poland' } },
+        //     { id: 2, fotoUrl: 'https://filestore.community.support.microsoft.com/api/images/f2e55cbf-8316-4d3a-9412-ecd8194b2a72?upload=true', followed: true, fullname: 'Sveta', status: 'I am a boss too', location: { city: 'Wrocław', country: 'Poland' } },
+        //     { id: 3, fotoUrl: 'https://filestore.community.support.microsoft.com/api/images/f2e55cbf-8316-4d3a-9412-ecd8194b2a72?upload=true', followed: false, fullname: 'Amina', status: 'I am a big boss', location: { city: 'Krakow', country: 'Poland' } }
+        // ]
+        // )
     }
 
     return <div>
