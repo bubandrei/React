935817db2c634533fff7ea77f234eac41eede829
a.bubelev <bubeleu@gmail.com>
2022-06-30 21:33:49 +0300

    avatar

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,46 +1,70 @@
+/* eslint-disable jsx-a11y/alt-text */
 import axios from "axios";
 import React from "react";
-import styles from './users.module.css';
+import styles from "./users.module.css";
+import userPhoto from '../../assets/images/user.png'
 
 let Users = (props) => {
+  if (props.users.length === 0) {
+    axios
+      .get("https://social-network.samuraijs.com/api/1.0/users")
+      .then((response) => props.setUsers(response.data.items));
+    // props.setUsers([
+    //     { id: 1, fotoUrl: 'https://filestore.community.support.microsoft.com/api/images/f2e55cbf-8316-4d3a-9412-ecd8194b2a72?upload=true', followed: false, fullname: 'Andrei', status: 'I am a boss', location: { city: 'Warszawa', country: 'Poland' } },
+    //     { id: 2, fotoUrl: 'https://filestore.community.support.microsoft.com/api/images/f2e55cbf-8316-4d3a-9412-ecd8194b2a72?upload=true', followed: true, fullname: 'Sveta', status: 'I am a boss too', location: { city: 'Wrocław', country: 'Poland' } },
+    //     { id: 3, fotoUrl: 'https://filestore.community.support.microsoft.com/api/images/f2e55cbf-8316-4d3a-9412-ecd8194b2a72?upload=true', followed: false, fullname: 'Amina', status: 'I am a big boss', location: { city: 'Krakow', country: 'Poland' } }
+    // ]
+    // )
+  }
 
-    if (props.users.length === 0) {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users')
-        .then((response)=> props.setUsers(response.data.items))
-        // props.setUsers([
-        //     { id: 1, fotoUrl: 'https://filestore.community.support.microsoft.com/api/images/f2e55cbf-8316-4d3a-9412-ecd8194b2a72?upload=true', followed: false, fullname: 'Andrei', status: 'I am a boss', location: { city: 'Warszawa', country: 'Poland' } },
-        //     { id: 2, fotoUrl: 'https://filestore.community.support.microsoft.com/api/images/f2e55cbf-8316-4d3a-9412-ecd8194b2a72?upload=true', followed: true, fullname: 'Sveta', status: 'I am a boss too', location: { city: 'Wrocław', country: 'Poland' } },
-        //     { id: 3, fotoUrl: 'https://filestore.community.support.microsoft.com/api/images/f2e55cbf-8316-4d3a-9412-ecd8194b2a72?upload=true', followed: false, fullname: 'Amina', status: 'I am a big boss', location: { city: 'Krakow', country: 'Poland' } }
-        // ]
-        // )
-    }
-
-    return <div>
-        {
-            props.users.map(user => <div key={user.id}>
-                <span>
-                    <div>
-                        <img src={user.fotoUrl} className={styles.userPhoto} />
-                    </div>
-                    <div>
-                        {user.followed
-                            ? <button onClick={() => { props.unfollow(user.id) }}>Follow</button>
-                            : <button onClick={() => { props.follow(user.id) }}>UnFollow</button>}
-                    </div>
-                </span>
-                <span>
-                    <span>
-                        <div>{user.name}</div>
-                        <div>{user.status}</div>
-                    </span>
-                    <span>
-                        <div>{'user.location.country'}</div>
-                        <div>{'user.location.city'}</div>
-                    </span>
-                </span>
+  return (
+    <div>
+      {props.users.map((user) => (
+        <div key={user.id}>
+          <span>
+            <div>
+              <img
+                src={
+                  user.photos.small != null
+                    ? user.photos.small
+                    : userPhoto
+                }
+                className={styles.userPhoto}
+              />
             </div>
-            )
-        }
+            <div>
+              {user.followed ? (
+                <button
+                  onClick={() => {
+                    props.unfollow(user.id);
+                  }}
+                >
+                  Follow
+                </button>
+              ) : (
+                <button
+                  onClick={() => {
+                    props.follow(user.id);
+                  }}
+                >
+                  UnFollow
+                </button>
+              )}
+            </div>
+          </span>
+          <span>
+            <span>
+              <div>{user.name}</div>
+              <div>{user.status}</div>
+            </span>
+            <span>
+              <div>{"user.location.country"}</div>
+              <div>{"user.location.city"}</div>
+            </span>
+          </span>
+        </div>
+      ))}
     </div>
-}
-export default Users
+  );
+};
+export default Users;
