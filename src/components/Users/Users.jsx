20c2fe31582de055bfa7d2df003082e50178965b
a.bubelev <bubeleu@gmail.com>
2022-07-02 20:41:26 +0300

    didMount

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -7,9 +7,11 @@ import userPhoto from "../../assets/images/user.png";
 class Users extends React.Component {
   constructor(props) {
     super(props);
+  }
+  componentDidMount() {
     axios
       .get("https://social-network.samuraijs.com/api/1.0/users")
-      .then((response) => props.setUsers(response.data.items))
+      .then((response) => this.props.setUsers(response.data.items));
   }
   render() {
     return (
