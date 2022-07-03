991cbbece467267e9937c9bb28747ae346dd04c0
a.bubelev <bubeleu@gmail.com>
2022-07-03 20:53:37 +0300

    current page

--- src/redux/usersReducer.js
+++ src/redux/usersReducer.js
@@ -7,7 +7,7 @@ let initialState = {
     users: [],
     pageSize: 5,
     totalUsersCount: 21,
-    currentPage: 5
+    currentPage: 1
 }
 
 const usersReducer = (state = initialState, action) => {
