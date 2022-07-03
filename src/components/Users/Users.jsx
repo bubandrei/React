e1b6fea0821b0eafddbf28f1be3c0b1c17019ee2
a.bubelev <bubeleu@gmail.com>
2022-07-03 20:45:48 +0300

    click current page

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -10,11 +10,15 @@ class Users extends React.Component {
   }
   componentDidMount() {
     axios
-      .get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`
+      )
       .then((response) => this.props.setUsers(response.data.items));
   }
   render() {
-    let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
+    let pagesCount = Math.ceil(
+      this.props.totalUsersCount / this.props.pageSize
+    );
     let pages = [];
     for (let i = 1; i <= pagesCount; i++) {
       pages.push(i);
@@ -23,9 +27,17 @@ class Users extends React.Component {
       <div>
         <div>
           {pages.map((p) => {
-           return <span className={this.props.currentPage === p && styles.selectedPage}>{p}</span>;
+            return (
+              <span
+                className={this.props.currentPage === p && styles.selectedPage}
+                onClick={() => {
+                  this.props.currentPage(p);
+                }}
+              >
+                {p}
+              </span>
+            );
           })}
-
         </div>
         {this.props.users.map((user) => (
           <div key={user.id}>
