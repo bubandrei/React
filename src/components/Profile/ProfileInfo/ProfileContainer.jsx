1f30c146014064f814c2fa6098825d0d659013bd
a.bubelev <bubeleu@gmail.com>
2022-07-17 13:49:35 +0300

    check userId

--- src/components/Profile/ProfileInfo/ProfileContainer.jsx
+++ src/components/Profile/ProfileInfo/ProfileContainer.jsx
@@ -7,22 +7,27 @@ import { withRouter } from "react-router-dom";
 
 class ProfileContainer extends React.Component {
   componentDidMount() {
+    let userId = this.props.match.params.userId;
+    if (userId !== undefined) {
+      userId = 2;
+    }
     axios
-      .get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+      .get(`https://social-network.samuraijs.com/api/1.0/profile/` + userId)
       .then((response) => {
         this.props.setUserProfile(response.data);
       });
   }
 
   render() {
-    return <Profile {...this.props} profile={this.props.profile}/>;
+    return <Profile {...this.props} profile={this.props.profile} />;
   }
 }
 let mapStateToProps = (state) => ({
   profile: state.profilePage.profile,
 });
 
-let WithUrlDataContainerComponent = withRouter(ProfileContainer)
+let WithUrlDataContainerComponent = withRouter(ProfileContainer);
 
-export default connect(mapStateToProps, { setUserProfile })(WithUrlDataContainerComponent = withRouter(ProfileContainer)
+export default connect(mapStateToProps, { setUserProfile })(
+  (WithUrlDataContainerComponent = withRouter(ProfileContainer))
 );
