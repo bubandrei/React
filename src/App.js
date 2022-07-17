5653cd5a77f84395b07709fac4f045c8306fd16e
a.bubelev <bubeleu@gmail.com>
2022-07-17 18:38:00 +0300

    ready withrouter

--- src/App.js
+++ src/App.js
@@ -22,7 +22,12 @@ const App = (props) => {
         <div className='app-wrapper-conent'>
           <Routes>
             <Route path="/dialogs/*" element={<DialogsContainer />} />
-            <Route path="/profile/:id" element={<ProfileContainer />} />
+            <Route path='/profile/:userId'
+              element={<ProfileContainer />}
+            />
+            <Route path='/profile'
+              element={<ProfileContainer />}
+            />
             <Route path="/users" element={<UsersContainer />} />
           </Routes>
         </div>
--- src/components/Profile/ProfileInfo/ProfileContainer.jsx
+++ src/components/Profile/ProfileInfo/ProfileContainer.jsx
@@ -5,18 +5,12 @@ import Profile from "../Profile";
 import { setUserProfile } from "../../../redux/profileReducer";
 import { useParams, useLocation, useNavigate } from "react-router-dom";
 
-
 function withRouter(Component) {
   function ComponentWithRouterProp(props) {
-      let location = useLocation();
-      let navigate = useNavigate();
-      let params = useParams();
-      return (
-          <Component
-              {...props}
-              router={{ location, navigate, params }}
-          />
-      );
+    let location = useLocation();
+    let navigate = useNavigate();
+    let params = useParams();
+    return <Component {...props} router={{ location, navigate, params }} />;
   }
 
   return ComponentWithRouterProp;
@@ -24,16 +18,15 @@ function withRouter(Component) {
 
 class ProfileContainer extends React.Component {
   componentDidMount() {
-    debugger
-    // let userId = this.props.match.params.userId;
-     let userId = this.props.router.params;
-    if (userId !== undefined){
+
+    let userId = this.props.router.params.userId;
+    if (!userId) {
       userId = 2;
     }
-
     axios
       .get(`https://social-network.samuraijs.com/api/1.0/profile/` + userId)
       .then((response) => {
+        console.log(response);
         this.props.setUserProfile(response.data);
       });
   }
@@ -49,5 +42,5 @@ let mapStateToProps = (state) => ({
 // let WithUrlDataContainerComponent = withRouter(ProfileContainer);
 
 export default connect(mapStateToProps, { setUserProfile })(
- withRouter(ProfileContainer)
+  withRouter(ProfileContainer)
 );
