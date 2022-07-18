bc86f1edf0fac4676e7e12ae4a6df9f4a663e521
a.bubelev <bubeleu@gmail.com>
2022-07-18 20:21:59 +0300

    authReducer

--- src/redux/authReducer.js
+++ src/redux/authReducer.js
@@ -7,3 +7,15 @@ let initialState = {
     email: null,
     login: null
 }
+
+const authReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case SET_USER_DATA:
+            return {
+                ...state,
+                ...action.data
+            }
+            default:
+                return state;
+    }
+}
