c039651acd4fce00b641f773107b3d9c94567e72
a.bubelev <bubeleu@gmail.com>
2022-07-18 20:27:31 +0300

    setUserData

--- src/redux/authReducer.js
+++ src/redux/authReducer.js
@@ -15,7 +15,10 @@ const authReducer = (state = initialState, action) => {
                 ...state,
                 ...action.data
             }
-            default:
-                return state;
+        default:
+            return state;
     }
 }
+
+export const setUserData = (userId, email, login) => ({ type: SET_USER_DATA, data: { userId, email, login } })
+export default authReducer;
