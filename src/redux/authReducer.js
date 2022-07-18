4aef0262c38a9174760056318835342c9954f5f1
a.bubelev <bubeleu@gmail.com>
2022-07-18 21:05:57 +0300

    isAuth

--- src/redux/authReducer.js
+++ src/redux/authReducer.js
@@ -5,7 +5,8 @@ const SET_USER_DATA = 'SET_USER_DATA';
 let initialState = {
     userId: null,
     email: null,
-    login: null
+    login: null,
+    isAuth: false
 }
 
 const authReducer = (state = initialState, action) => {
@@ -13,12 +14,13 @@ const authReducer = (state = initialState, action) => {
         case SET_USER_DATA:
             return {
                 ...state,
-                ...action.data
+                ...action.data,
+                isAuth: true
             }
         default:
             return state;
     }
 }
 
-export const setUserData = (userId, email, login) => ({ type: SET_USER_DATA, data: { userId, email, login } })
+export const setAuthUserData = (userId, email, login) => ({ type: SET_USER_DATA, data: { userId, email, login } })
 export default authReducer;
