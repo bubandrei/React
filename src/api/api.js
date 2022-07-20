e0c59888a40f1bc3b3ba06bd2dfddc7768a10937
a.bubelev <bubeleu@gmail.com>
2022-07-20 21:54:59 +0300

    baseUrl

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,11 @@
 import * as axios from "axios";
 
+const baseUrl = 'https://social-network.samuraijs.com/api/1.0/';
+
 export const getUsers = (currentPage = 1, pageSize = 10) => {
     return axios
         .get(
-            `https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`,
+            baseUrl + `users?page=${currentPage}&count=${pageSize}`,
             { withCredentials: true }
         )
         .then(response => {
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -19,18 +19,18 @@ class UsersContainer extends React.Component {
   }
   componentDidMount() {
     this.props.toggleIsFetching(true);
-    getUsers(this.props.currentPage, this.props.pageSize).then((response) => {
+    getUsers(this.props.currentPage, this.props.pageSize).then((data) => {
       this.props.toggleIsFetching(false);
-      this.props.setUsers(response.data.items);
-      this.props.setTotalUsersCount(response.data.totalCount);
+      this.props.setUsers(data.items);
+      this.props.setTotalUsersCount(data.totalCount);
     });
   }
   onPageChanged = (pageNumber) => {
     this.props.setCurrentPage(pageNumber);
     this.props.toggleIsFetching(true);
-    getUsers(pageNumber, this.props.pageSize).then((response) => {
+    getUsers(pageNumber, this.props.pageSize).then((data) => {
       this.props.toggleIsFetching(false);
-      this.props.setUsers(response.data.items);
+      this.props.setUsers(data.items);
     });
   };
   render() {
