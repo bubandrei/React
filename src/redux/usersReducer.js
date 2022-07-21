e62aba11a975f876dd35c2ad9ccf5c67a3b7f84f
a.bubelev <bubeleu@gmail.com>
2022-07-21 20:53:33 +0300

    ac

--- src/redux/usersReducer.js
+++ src/redux/usersReducer.js
@@ -4,6 +4,7 @@ const SET_USERS = 'SET_USERS';
 const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
 const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_UERS_COUNT';
 const TOGGLE_IS_FETCHING = 'TOGGLE_IS_FETCHING';
+const TOGGLE_IS_FOLLOWING_PROGRESS = 'TOGGLE_IS_FOLLOWING_PROGRESS';
 
 let initialState = {
     users: [],
@@ -42,8 +43,10 @@ const usersReducer = (state = initialState, action) => {
             return { ...state, currentPage: action.currentPage }
         case SET_TOTAL_USERS_COUNT:
             return { ...state, totalUsersCount: action.count }
-            case TOGGLE_IS_FETCHING:
-                return { ...state, isFetching: action.isFetching }
+        case TOGGLE_IS_FETCHING:
+            return { ...state, isFetching: action.isFetching }
+        case TOGGLE_IS_FOLLOWING_PROGRESS:
+            return { ...state, followingInProgress: action.isFetching }
         default:
             return state;
     }
@@ -54,5 +57,6 @@ export const setUsers = (users) => { return { type: SET_USERS, users } };
 export const setCurrentPage = (currentPage) => { return { type: SET_CURRENT_PAGE, currentPage: currentPage } };
 export const setTotalUsersCount = (totalUsersCount) => { return { type: SET_TOTAL_USERS_COUNT, count: totalUsersCount } }
 export const toggleIsFetching = (isFetching) => { return { type: TOGGLE_IS_FETCHING, isFetching } };
+export const toggleFollowingProgress = (isFetching) => { return { type: TOGGLE_IS_FOLLOWING_PROGRESS, isFetching } };
 
 export default usersReducer
