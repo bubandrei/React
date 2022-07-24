0b4fcdcabe2ff13b408e8bfea0c47d5935c7646a
a.bubelev <bubeleu@gmail.com>
2022-07-24 14:22:38 +0300

    authAPI

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -4,23 +4,24 @@ import Header from "./Header";
 import * as axios from "axios";
 import { connect } from "react-redux";
 import { setAuthUserData } from "../../redux/authReducer";
+import { authAPI } from "../../api/api";
 
 class HeaderContainer extends React.Component {
   componentDidMount() {
-   me.then((response) => {
-        if (response.data.resultCode === 0) {
-          let { id, email, login } = response.data.data;
-          this.props.setAuthUserData(id, email, login);
-        }
-      });
+    authAPI.me().then((response) => {
+      if (response.data.resultCode === 0) {
+        let { id, email, login } = response.data.data;
+        this.props.setAuthUserData(id, email, login);
+      }
+    });
   }
   render() {
     return <Header {...this.props} />;
   }
 }
 const mapStateToProps = (state) => ({
-    isAuth: state.auth.isAuth,
-    login: state.auth.login
+  isAuth: state.auth.isAuth,
+  login: state.auth.login,
 });
 
 export default connect(mapStateToProps, { setAuthUserData })(HeaderContainer);
