05b1d95ab37d2c2a44cfdec4c8455a7d0ed528ec
a.bubelev <bubeleu@gmail.com>
2022-07-25 21:35:57 +0300

    ready ConnectedAuthRedirectComponent

--- src/hoc/withAuthRedirect.js
+++ src/hoc/withAuthRedirect.js
@@ -14,9 +14,8 @@ export const withAuthRedirect = (Component) => {
         }
     }
 
-    let ConnectedAuthRedirectComponent = connect(
-        mapStateToPropsForRedirect(RedirectComponent)
-    );
+    let ConnectedAuthRedirectComponent = connect
+        (mapStateToPropsForRedirect)(RedirectComponent);
 
     return ConnectedAuthRedirectComponent;
 }
