148b66e6cf003714106e60431d7337ddc9cda6ff
a.bubelev <bubeleu@gmail.com>
2022-07-25 19:41:52 +0300

    DialogsContainer isAuth

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -27,7 +27,8 @@ import { connect } from 'react-redux';
 // }
 let mapStateToProps = (state) => {
     return {
-        dialogsPage: state.dialogsPage
+        dialogsPage: state.dialogsPage,
+        isAuth: state.auth.isAuth
     }
 }
 let mapDispatchToProps = (dispatch) => {
