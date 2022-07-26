61bc45098318da33137fca2ce50767a13cb58826
a.bubelev <bubeleu@gmail.com>
2022-07-26 11:22:02 +0300

    onblur deActivate

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -4,20 +4,33 @@ class ProfileStatus extends React.Component {
   state = {
     editMode: false,
   };
-  activateEditMode (){
-    this.state.editMode = true;
-  };
+  activateEditMode() {
+    this.setState({
+      editMode: true,
+    });
+  }
+  deActivateEditMode() {
+    this.setState({
+      editMode: false,
+    });
+  }
   render() {
     return (
       <div>
         {!this.state.editMode && (
           <div>
-            <span onClick={this.activateEditMode.bind(this)}>{this.props.status}</span>
+            <span onClick={this.activateEditMode.bind(this)}>
+              {this.props.status}
+            </span>
           </div>
         )}
         {this.state.editMode && (
           <div>
-            <input type="text" value={this.props.status} />
+            <input
+              onBlur={this.deActivateEditMode.bind(this)}
+              type="text"
+              value={this.props.status}
+            />
           </div>
         )}
       </div>
