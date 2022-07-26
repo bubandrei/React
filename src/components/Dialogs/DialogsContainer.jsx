340b1a1a9135c56b65f8294d1772b78402c1b557
a.bubelev <bubeleu@gmail.com>
2022-07-26 11:28:39 +0300

    delete part script in dialogscontainer

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -5,29 +5,6 @@ import { Navigate } from 'react-router-dom';
 import { withAuthRedirect } from '../../hoc/withAuthRedirect';
 import { compose } from 'redux';
 
-// const DialogsContainer = (props) => {
-//     return (
-//         <StoreContext.Consumer>{
-//             (store) => {
-//                 let state = store.getState().dialogsReducer;
-
-//                 let onSendMessageClick = () => {
-//                     store.dispatch(sendMessageCreator())
-//                 }
-//                 let onNewMessageChange = (body) => {
-//                     store.dispatch(updateNewMessageBodyCreator(body))
-//                 }
-
-//                 return <Dialogs
-//                     updateNewMessageBody={onNewMessageChange}
-//                     sendMessage={onSendMessageClick}
-//                     dialogsPage={state} />
-//             }
-//         }
-
-//         </StoreContext.Consumer>
-//     )
-// }
 let mapStateToProps = (state) => {
     return {
         dialogsPage: state.dialogsPage,
