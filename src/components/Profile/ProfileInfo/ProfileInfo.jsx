e26b80d9213e9a2e95b1eb344477bec7e37a7f07
a.bubelev <bubeleu@gmail.com>
2022-07-27 14:50:00 +0300

    fix props

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -15,7 +15,7 @@ const ProfileInfo = (props) => {
       </div>
       <div className={s.discriptionBlock}>
         <img src={props.profile.photos.large} alt="" />
-        <ProfileStatus status={props.status}/>
+        <ProfileStatus status={props.status} updateStatus={props.updateStatus}/>
       </div>
     </div>
   );
