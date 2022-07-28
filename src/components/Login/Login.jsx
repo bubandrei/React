cb9013f1fbffd26fda0c10fe102b8d821b26cc3b
bubandrei <bubeleu@gmail.com>
2022-07-28 21:27:55 +0300

    input form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,31 @@
 import React from "react";
 
-const Login = () =>{
-    return(
-        <h1>LOGIN</h1>
+const LoginForm = () => {
+    return (
+        <form>
+            <div>
+                <input type="text" placeholder={'Login'} />
+            </div>
+            <div>
+                <input type="text" placeholder={'Password'} />
+            </div>
+            <div>
+                <input type={'checkbox'} /> remeber me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
+    )
+}
+
+const Login = () => {
+    return (
+        <>
+            <h1>LOGIN</h1>
+            <LoginForm />
+        </>
+
     )
 }
 
