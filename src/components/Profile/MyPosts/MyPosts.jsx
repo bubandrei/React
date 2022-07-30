7a62206bce21aa9275b345b685b0276f02059232
a.bubelev <bubeleu@gmail.com>
2022-07-30 15:23:25 +0300

    field for post

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,41 +1,48 @@
-import React from 'react';
-import s from './MyPosts.module.css';
-import Post from './Post/Post';
-import { addPostActionCreate, updateNewPostTextActionCreator } from '../../../redux/profileReducer';
-
-
+import React from "react";
+import s from "./MyPosts.module.css";
+import Post from "./Post/Post";
+import {
+  addPostActionCreate,
+  updateNewPostTextActionCreator,
+} from "../../../redux/profileReducer";
+import { Field, reduxForm } from "redux-form";
 
 const MyPosts = (props) => {
-    let postsElements = props.posts.map((el, index) => {
-        return <Post key={index} message={el.message} count={el.count} id={el.id} />
-    })
-
-    let newPostElement = React.createRef();
-    let onAddPost = () => {
-        props.addPost();
-
-    }
-
-    let onPostChange = () => {
-        let text = newPostElement.current.value;
-        props.updateNewPostText(text);
-    }
+  let postsElements = props.posts.map((el, index) => {
     return (
-        <div className={s.postsBlock}>
-            <h3>My posts</h3>
-            <div>
-                <div>
-                    <textarea onChange={onPostChange} ref={newPostElement} value={props.newPostText} />
-                </div>
-                <div>
-                    <button onClick={onAddPost}>Add post</button>
-                </div>
-            </div>
-            <div className={s.posts}>
-                {postsElements}
-            </div>
+      <Post key={index} message={el.message} count={el.count} id={el.id} />
+    );
+  });
 
+  let newPostElement = React.createRef();
+  let onAddPost = () => {
+    props.addPost();
+  };
+
+  let onPostChange = () => {
+    let text = newPostElement.current.value;
+    props.updateNewPostText(text);
+  };
+  return (
+    <form onSubmit={props.hadleSubmit}>
+      <div className={s.postsBlock}>
+        <h3>My posts</h3>
+        <div>
+          <div>
+            <Field
+              name={"post"}
+              onChange={onPostChange}
+              ref={newPostElement}
+              value={props.newPostText}
+            />
+          </div>
+          <div>
+            <button onClick={onAddPost}>Add post</button>
+          </div>
         </div>
-    )
-}
-export default MyPosts
+        <div className={s.posts}>{postsElements}</div>
+      </div>
+    </form>
+  );
+};
+export default MyPosts;
