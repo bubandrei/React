ba5728b1897b1cf1cdebc225603f228d7153247f
a.bubelev <bubeleu@gmail.com>
2022-07-31 19:40:06 +0300

    css for formControl

--- src/components/common/FormControls/FormsControl.js
+++ src/components/common/FormControls/FormsControl.js
@@ -1,8 +1,9 @@
 import React from "react";
+import styles from "./FormsControls.module.css"
 
 export const Textarea = ({ input, meta, ...props }) => {
     return (
-        <div>
+        <div className={styles.formControl}>
             <textarea {...input} {...props} />
         </div>
     )
