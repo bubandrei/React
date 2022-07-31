f4e8c5a470b1d5813c00bb7cd57e33cf4570ab8f
a.bubelev <bubeleu@gmail.com>
2022-07-31 20:24:48 +0300

    validate form ready

--- src/components/common/FormControls/FormsControl.js
+++ src/components/common/FormControls/FormsControl.js
@@ -1,28 +1,31 @@
 import React from "react";
 import styles from "./FormsControls.module.css";
 
-
-
-export const Textarea = ({ input, meta, ...props }) => {
+export const FormControl = ({ input, meta, ...props }) => {
     const hasError = meta.touched && meta.error;
     return (
         <div className={styles.formControl + ' ' + (hasError ? styles.error : '')}>
             <div>
-                <textarea {...input} {...props} />
+                {props.children}
             </div>
             {hasError && <span>{meta.error}</span>}
         </div>
     )
 }
 
-export const Input = ({ input, meta, ...props }) => {
-    const hasError = meta.touched && meta.error;
+
+
+
+export const Textarea = (props) => {
+    const { input, meta, child, ...restProps } = props;
     return (
-        <div className={styles.formControl + ' ' + (hasError ? styles.error : '')}>
-            <div>
-                <input {...input} {...props} />
-            </div>
-            {hasError && <span>{meta.error}</span>}
-        </div>
+        <FormControl {...props}><textarea {...input} {...restProps} /></FormControl>
+    )
+}
+
+export const Input = (props) => {
+    const { input, meta, child, ...restProps } = props;
+    return (
+        <FormControl {...props}><input {...input} {...restProps} /></FormControl>
     )
 }
