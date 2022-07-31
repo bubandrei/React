c6613cdfed64e9eff713f97eca80c2e954f8e243
a.bubelev <bubeleu@gmail.com>
2022-07-31 19:46:43 +0300

    span for text error

--- src/components/common/FormControls/FormsControl.js
+++ src/components/common/FormControls/FormsControl.js
@@ -1,10 +1,13 @@
 import React from "react";
-import styles from "./FormsControls.module.css"
+import styles from "./FormsControls.module.css";
 
 export const Textarea = ({ input, meta, ...props }) => {
     return (
-        <div className={styles.formControl}>
+        <div className={styles.formControl + ' ' + styles.error}>
+            <div>
             <textarea {...input} {...props} />
+            </div>
+           <span>"some error"</span>
         </div>
     )
 }
