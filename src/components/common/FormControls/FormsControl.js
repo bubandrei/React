b21145b02da76612383b39ce7d7c9c90a2b86ff3
a.bubelev <bubeleu@gmail.com>
2022-07-31 19:52:34 +0300

    fix error

--- src/components/common/FormControls/FormsControl.js
+++ src/components/common/FormControls/FormsControl.js
@@ -1,11 +1,12 @@
 import React from "react";
 import styles from "./FormsControls.module.css";
 
-const hasError = meta.touched && meta.error;
+
 
 export const Textarea = ({ input, meta, ...props }) => {
+    const hasError = meta.touched && meta.error;
     return (
-        <div className={styles.formControl + ' ' + hasError && styles.error}>
+        <div className={styles.formControl + ' ' + (hasError ? styles.error : '')}>
             <div>
                 <textarea {...input} {...props} />
             </div>
