2348d26eca07e132b55d29e495871ebab3835e51
a.bubelev <bubeleu@gmail.com>
2022-07-31 19:53:37 +0300

    created FormsControl

--- src/components/common/FormControls/FormsControl.js
+++ src/components/common/FormControls/FormsControl.js
@@ -10,7 +10,7 @@ export const Textarea = ({ input, meta, ...props }) => {
             <div>
                 <textarea {...input} {...props} />
             </div>
-            {hasError && <span>"some error"</span>}
+            {hasError && <span>{meta.error}</span>}
         </div>
     )
 }
