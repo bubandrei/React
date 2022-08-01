f6238cec12d648fd77f83a6abb40217ce4c07d0b
a.bubelev <bubeleu@gmail.com>
2022-08-01 13:11:05 +0300

    button logout for header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,7 +8,9 @@ const Header = (props) => {
       <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/NASA_logo.svg/800px-NASA_logo.svg.png"></img>
 
       <div className={s.loginBlock}>
-        {props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink>}
+        {props.isAuth ? 
+        <div>{props.login} - <button onClick={props.logout}>Log out</button></div>
+        : <NavLink to={'/login'}>Login</NavLink>}
       </div>
     </header>
   );
