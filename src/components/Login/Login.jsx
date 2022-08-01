e200daa1d7eb3c5a395039738c0fbea665d1ddf8
a.bubelev <bubeleu@gmail.com>
2022-08-01 13:04:19 +0300

    login redirect to profile

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -7,40 +7,54 @@ import { Input } from "../common/FormControls/FormsControl";
 import { login } from "../../redux/authReducer";
 
 const LoginForm = (props) => {
-    return (
-        <form onSubmit={props.handleSubmit}>
-            <div>
-                <Field name={"email"} placeholder={'Email'} component={Input} validate={[required]}/>
-            </div>
-            <div>
-                <Field  name={"password"} placeholder={'Password'} component={Input} validate={[required]} type={"password"}/>
-            </div>
-            <div>
-                <Field name={"remeber me"} type={'checkbox'} component={"input"}/> remeber me
-            </div>
-            <div>
-                <button>Login</button>
-            </div>
-        </form>
-    )
-}
+  return (
+    <form onSubmit={props.handleSubmit}>
+      <div>
+        <Field
+          name={"email"}
+          placeholder={"Email"}
+          component={Input}
+          validate={[required]}
+        />
+      </div>
+      <div>
+        <Field
+          name={"password"}
+          placeholder={"Password"}
+          component={Input}
+          validate={[required]}
+          type={"password"}
+        />
+      </div>
+      <div>
+        <Field name={"remeber me"} type={"checkbox"} component={"input"} />{" "}
+        remeber me
+      </div>
+      <div>
+        <button>Login</button>
+      </div>
+    </form>
+  );
+};
 
 const LoginReduxForm = reduxForm({
-    form: 'login'
+  form: "login",
 })(LoginForm);
 
 const Login = (props) => {
-    const onSubmit = (formData) =>{
-        props.login(formData.email, formData.password, formData.remeberMe)
-    }
+  const onSubmit = (formData) => {
+    props.login(formData.email, formData.password, formData.remeberMe);
+  };
+  if (props.isAuth) {
+    return <Redirect to={"/profile"} />;
+  }
 
-    return (
-        <>
-            <h1>LOGIN</h1>
-            <LoginReduxForm onSubmit={onSubmit}/>
-        </>
+  return (
+    <>
+      <h1>LOGIN</h1>
+      <LoginReduxForm onSubmit={onSubmit} />
+    </>
+  );
+};
 
-    )
-}
-
-export default connect(null,{login})(Login)
+export default connect(null, { login })(Login);
