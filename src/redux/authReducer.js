51d64cb39182a3226c4204d1e1140bb4155492dc
a.bubelev <bubeleu@gmail.com>
2022-08-01 12:39:29 +0300

    logout for authReducer

--- src/redux/authReducer.js
+++ src/redux/authReducer.js
@@ -15,7 +15,7 @@ const authReducer = (state = initialState, action) => {
         case SET_USER_DATA:
             return {
                 ...state,
-                ...action.data,
+                ...action.payload,
                 isAuth: true
             }
         default:
@@ -23,12 +23,12 @@ const authReducer = (state = initialState, action) => {
     }
 }
 
-export const setAuthUserData = (userId, email, login) => ({ type: SET_USER_DATA, data: { userId, email, login } })
+export const setAuthUserData = (userId, email, login, isAuth) => ({ type: SET_USER_DATA, payload: { userId, email, login, isAuth } })
 export const getAuthUserData = () => (dispatch) => {
     authAPI.me().then((response) => {
         if (response.data.resultCode === 0) {
             let { id, email, login } = response.data.data;
-            dispatch(setAuthUserData(id, email, login));
+            dispatch(setAuthUserData(id, email, login, true));
         }
     });
 }
@@ -36,6 +36,13 @@ export const login = (email, password, remeberMe) => (dispatch) => {
     authAPI.login(email, password, remeberMe).then((response) => {
         if (response.data.resultCode === 0) {
             dispatch(getAuthUserData())
+        };
+    })
+}
+export const logout = () => (dispatch) => {
+    authAPI.logout().then((response) => {
+        if (response.data.resultCode === 0) {
+            dispatch(setAuthUserData(null, null, null, false));
         };
     })
 }
