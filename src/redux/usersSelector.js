ab7924a1e39493ea55fe3a625b820dbce630f313
a.bubelev <bubeleu@gmail.com>
2022-08-02 19:21:40 +0300

    functions for usersSelector

--- src/redux/usersSelector.js
+++ src/redux/usersSelector.js
@@ -1,3 +1,28 @@
-export const getAllUsers = (state) =>{
+export const getAllUsers = (state) => {
     return state.usersPage.users
 }
+export const getPageSize = (state) => {
+    return state.usersPage.pageSize
+}
+export const getTotalUserCount = (state) => {
+    return state.usersPage.totalUsersCount
+}
+export const getCurrentPage = (state) => {
+    return state.usersPage.currentPage
+}
+export const getIsFetching = (state) => {
+    return state.usersPage.isFetching
+}
+export const getFollowingInProgress = (state) => {
+    return state.usersPage.followingInProgress
+}
+
+
+
+
+    // users: getAllUsers(state),
+    // pageSize: state.usersPage.pageSize,
+    // totalUsersCount: state.usersPage.totalUsersCount,
+    // currentPage: state.usersPage.currentPage,
+    // isFetching: state.usersPage.isFetching,
+    // followingInProgress: state.usersPage.followingInProgress,
