b7c8fb5f7bceb4bf7bcd2d94221e5ce847193a08
a.bubelev <bubeleu@gmail.com>
2022-08-02 13:44:52 +0300

    rewrite class App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
@@ -9,26 +9,29 @@ import HeaderContainer from './components/Header/HeaderContainer';
 import Login from './components/Login/Login';
 
 
-const App = (props) => {
-  return (
-    <BrowserRouter>
-      <div className='app-wrapper'>
-        <HeaderContainer />
-        <Navbar
-        // state={props.state.dialogsPage} 
-        />
-        <div className='app-wrapper-conent'>
-          <Routes>
-            <Route path="/dialogs/*" element={<DialogsContainer />} />
-            <Route path='/profile/:userId' element={<ProfileContainer />} />
-            <Route path='/profile' element={<ProfileContainer />} />
-            <Route path="/users" element={<UsersContainer />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
+class App extends Component {
+  componentDidMount() {
+    this.props.getAuthUserData();
+  }
+  render(){
+    return (
+      <BrowserRouter>
+        <div className='app-wrapper'>
+          <HeaderContainer />
+          <Navbar/>
+          <div className='app-wrapper-conent'>
+            <Routes>
+              <Route path="/dialogs/*" element={<DialogsContainer />} />
+              <Route path='/profile/:userId' element={<ProfileContainer />} />
+              <Route path='/profile' element={<ProfileContainer />} />
+              <Route path="/users" element={<UsersContainer />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </div>
         </div>
-      </div>
-    </BrowserRouter>
-  );
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -4,9 +4,7 @@ import { connect } from "react-redux";
 import { getAuthUserData, logout } from "../../redux/authReducer";
 
 class HeaderContainer extends React.Component {
-  componentDidMount() {
-    this.props.getAuthUserData();
-  }
+
   render() {
     return <Header {...this.props} />;
   }
@@ -16,4 +14,4 @@ const mapStateToProps = (state) => ({
   login: state.auth.login,
 });
 
-export default connect(mapStateToProps, { getAuthUserData, logout })(HeaderContainer);
+export default connect(mapStateToProps, { logout })(HeaderContainer);
