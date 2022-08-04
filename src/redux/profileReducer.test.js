4e0b6f5b5da7d12710b3a467435bb8b9b558887f
a.bubelev <bubeleu@gmail.com>
2022-08-04 22:50:05 +0300

    expect

--- src/redux/profileReducer.test.js
+++ src/redux/profileReducer.test.js
@@ -1,8 +1,17 @@
 import React from "react";
-import profileReducer, {addPostActionCreate} from '../redux/profileReducer';
+import profileReducer, { addPostActionCreate } from '../redux/profileReducer';
 
-it('new post should be added',()=>{
-    let action = addPostActionCreate();
-    let newState = profileReducer({},{})
-
+let state = {
+    // 1. test data 
+    posts: [
+        { message: 'It\'s my first post!?!?!', count: 44, id: '1' },
+        { message: 'Hi, how are you?', count: 77, id: '2' },
+    ]
+}
+// 2. action
+it('new post should be added', () => {
+    let action = addPostActionCreate('My new post');
+    let newState = profileReducer(state, action)
 })
+// 3. expect
+newState.posts.length === 3 
