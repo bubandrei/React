ab538a2be050b929bd74d0cf71b391ea5a8f2268
a.bubelev <bubeleu@gmail.com>
2022-08-05 14:04:28 +0300

    async for updateStatus

--- src/redux/profileReducer.js
+++ src/redux/profileReducer.js
@@ -55,11 +55,10 @@ export const getStatus = (userId) => async (dispatch) => {
     let response = await profileAPI.getStatus(userId);
     dispatch(setStatus(response.data));
 }
-export const updateStatus = (status) => (dispatch) => {
-    profileAPI.updateStatus(status).then((response) => {
+export const updateStatus = (status) => async (dispatch) => {
+    let response = await profileAPI.updateStatus(status);
         if (response.data.resultCode === 0) {
             dispatch(setStatus(status));
         }
-    });
 }
 export default profileReducer
