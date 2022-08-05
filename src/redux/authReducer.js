1a97948eeac9dd16f240ac41f6d1c2c2f6506471
a.bubelev <bubeleu@gmail.com>
2022-08-05 13:40:55 +0300

    async for login in authReducer

--- src/redux/authReducer.js
+++ src/redux/authReducer.js
@@ -31,15 +31,14 @@ export const getAuthUserData = () => async (dispatch) => {
         dispatch(setAuthUserData(id, email, login, true));
     }
 }
-export const login = (email, password, remeberMe) => (dispatch) => {
-    authAPI.login(email, password, remeberMe).then((response) => {
-        if (response.data.resultCode === 0) {
-            dispatch(getAuthUserData())
-        } else {
-            let message = response.data.message > 0 ? response.data.message[0] : "Some error";
-            dispatch(stopSubmit("login", { _error: message }));
-        }
-    })
+export const login = (email, password, remeberMe) => async (dispatch) => {
+    let response = await authAPI.login(email, password, remeberMe);
+    if (response.data.resultCode === 0) {
+        dispatch(getAuthUserData())
+    } else {
+        let message = response.data.message > 0 ? response.data.message[0] : "Some error";
+        dispatch(stopSubmit("login", { _error: message }));
+    }
 }
 export const logout = () => (dispatch) => {
     authAPI.logout().then((response) => {
