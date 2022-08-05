6e8f9d1c4098c761163195c91f1acc282a9b4fe3
a.bubelev <bubeleu@gmail.com>
2022-08-05 13:38:52 +0300

    async await for authReducer

--- src/redux/authReducer.js
+++ src/redux/authReducer.js
@@ -2,7 +2,7 @@ import React from "react";
 import { stopSubmit } from "redux-form";
 import { authAPI } from "../api/api";
 
-const SET_USER_DATA = 'SET_USER_DATA';
+const SET_USER_DATA = 'my-network/auth/SET_USER_DATA';
 
 let initialState = {
     userId: null,
@@ -24,13 +24,12 @@ const authReducer = (state = initialState, action) => {
 }
 
 export const setAuthUserData = (userId, email, login, isAuth) => ({ type: SET_USER_DATA, payload: { userId, email, login, isAuth } })
-export const getAuthUserData = () => (dispatch) => {
-   return  authAPI.me().then((response) => {
-        if (response.data.resultCode === 0) {
-            let { id, email, login } = response.data.data;
-            dispatch(setAuthUserData(id, email, login, true));
-        }
-    });
+export const getAuthUserData = () => async (dispatch) => {
+    let response = await authAPI.me();
+    if (response.data.resultCode === 0) {
+        let { id, email, login } = response.data.data;
+        dispatch(setAuthUserData(id, email, login, true));
+    }
 }
 export const login = (email, password, remeberMe) => (dispatch) => {
     authAPI.login(email, password, remeberMe).then((response) => {
