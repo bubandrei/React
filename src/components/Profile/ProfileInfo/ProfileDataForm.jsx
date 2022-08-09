3ce589f62300c53d9e04aa1d8f3867130e5efdfd
a.bubelev <bubeleu@gmail.com>
2022-08-09 22:19:31 +0300

    handlesubmit for profiledataform

--- src/components/Profile/ProfileInfo/ProfileDataForm.jsx
+++ src/components/Profile/ProfileInfo/ProfileDataForm.jsx
@@ -11,7 +11,7 @@ const ProfileDataForm = ({ handleSubmit, profile, goToEditMode }) => {
   return (
     <form onSubmit={handleSubmit}>
       <div>
-        <button onClick={() => {}}>Save</button>
+        <button onClick={handleSubmit}>Save</button>
       </div>
       <div>
         <b>Full name</b>:{createField("Full name", "fullName", [], Input)}
