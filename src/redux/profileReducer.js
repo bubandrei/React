0697632cccd081400fcdf760a69c30c60cb64a22
a.bubelev <bubeleu@gmail.com>
2022-08-10 20:17:31 +0300

    debbug

--- src/redux/profileReducer.js
+++ src/redux/profileReducer.js
@@ -75,7 +75,7 @@ export const savePhoto = (file) => async (dispatch) => {
 export const saveProfile = (profile) => async (dispatch) => {
     let response = await profileAPI.saveProfile(profile);
     if (response.data.resultCode === 0) {
-        dispatch(savePhotoSucces(response.data.data.photos));
+        // dispatch(savePhotoSucces(response.data.data.photos));
     }
 }
 export default profileReducer
